a20d03f46ca382d69a94e7cb5d21ff6dba641fca
Stella Maia <stellamaia97@hotmail.com>
2024-03-08 11:44:37 -0300

    Implementação de Particles

--- src/main.js
+++ src/main.js
@@ -5,10 +5,17 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
+import Particles from "@tsparticles/vue3";
+import { loadSlim } from "@tsparticles/slim";
 loadFonts()
 
 createApp(App)
   .use(router)
   .use(store)
   .use(vuetify)
+  .use(Particles, {
+    init: async (engine) => {
+      await loadSlim(engine);
+    },
+  })
   .mount('#app')
