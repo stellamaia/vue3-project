b5468421fa47c351c379250d12f9d6ef35663004
Stella Maia <stellamaia97@hotmail.com>
2024-03-09 00:12:28 -0300

    Armazenando Favorites em localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,22 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    favorites: JSON.parse(localStorage.getItem('favorites')) || [],
   },
   getters: {
+    favorites: state => state.favorites,
+    isFavorite: state => item => state.favorites.some(fav => fav.date === item.date),
   },
   mutations: {
+    toggleFavorite(state, item) {
+      const index = state.favorites.findIndex(fav => fav.date === item.date);
+      if (index === -1) {
+        state.favorites.push(item);
+      } else {
+        state.favorites.splice(index, 1);
+      }
+      localStorage.setItem('favorites', JSON.stringify(state.favorites));
+    },
   },
   actions: {
   },
