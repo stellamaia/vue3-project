570a949cfec83aab51e26016342a7e8e889bb7e6
Stella Maia <stellamaia97@hotmail.com>
2024-03-11 15:04:24 -0300

    Implementação de novas rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
-
 const routes = [
  {
    path: '/',
@@ -11,7 +9,6 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-   
     component: () => import( '../views/AboutView.vue')
   }
 ]
