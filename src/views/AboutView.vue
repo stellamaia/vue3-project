8f8b6241857c16bc8aae3ae59afe1fe5e193d6e3
Stella Maia <stellamaia97@hotmail.com>
2024-03-12 00:02:29 -0300

    Condição para caso não tenha item

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,19 +1,28 @@
 <template>
-  <div style="  z-index: 1;  position: relative;"> 
+  <div style="  z-index: 1;  position: relative;">
     <div class="app container" ref="scrollArea" @scroll="checkScroll">
       <NavBar />
-            <div class="title-container">
-                <h1  style=" color: white;  font-weight: bold;  text-align: center; padding-top: 50px;">
-                  Favorite images
-                  </h1>
-            </div>
-      <div >
-        <CardComponent :items="favoriteItems"/>
+      <div class="title-container">
+        <h1 class="favorite-card-title">
+          Favorite Cards
+        </h1>
+      </div>
+      <div>
+        <CardComponent v-if="favoriteItems.length" :items="favoriteItems" />
+
+      </div>
+      <div class="no-card">
+        <p v-if="!favoriteItems.length">
+          <span class="material-symbols-outlined">
+            no_photography
+          </span>
+        </p>
+
       </div>
     </div>
   </div>
 </template>
-  
+
 <script>
 import { ref, onMounted } from "vue";
 
@@ -28,33 +37,50 @@ export default {
   setup() {
     onMounted(() => {
       getFavoriteItems();
-      
     });
 
     const favoriteItems = ref([]);
-  
-      const getFavoriteItems = () => {
-        const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-        const uniqueFavorites = favorites.reduce((acc, curr) => {
-          if (!acc.some(item => item.title === curr.title)) {
-            acc.push(curr);
-          }
-          return acc;
-        }, []);
-        favoriteItems.value = uniqueFavorites;
-      };
-
-    
+    const getFavoriteItems = () => {
+      const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+      const uniqueFavorites = favorites.reduce((acc, curr) => {
+        if (!acc.some(item => item.title === curr.title)) {
+          acc.push(curr);
+        }
+        return acc;
+      }, []);
+      favoriteItems.value = uniqueFavorites;
+    };
     return { favoriteItems };
   },
 };
 </script>
-  
-  <style>
 
+<style>
 .title-container {
   z-index: -1;
-  position: static;  padding-top: 50px;
+  position: static;
+  padding-top: 50px;
 }
 
+.no-card {
+  text-align: center;
+  font-size: 50px;
+  color: white;
+  top: 300px;
+  position: absolute;
+  width: 100%;
+}
+
+.material-symbols-outlined {
+  color: #7e7878;
+  font-size: 60px !important;
+
+}
+
+.favorite-card-title {
+  color: white;
+  font-weight: bold;
+  text-align: center;
+  padding-top: 50px;
+}
 </style>
